1677341532659864ca97e03c285947dca55d41e6
Avielbe <avielmbe@gmail.com>
2024-06-18 17:11:57 +0300

    Deleting unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -16,24 +16,3 @@ function App() {
 
 export default App;
 
-
-// import './App.css'
-// import Header from './components/Header/Header';
-// import Cart from './components/Cart';
-// import ProductsScreen from './components/ProductsScreen';
-// import { CartProvider } from './components/CartContext';
-
-// function App() {
-
-//   return (
-//     <CartProvider>
-//     <>
-//     <Header/>
-//     <Cart/>
-//     <ProductsScreen />
-//     </>
-//     </CartProvider>
-//   )
-// }
-
-// export default App
